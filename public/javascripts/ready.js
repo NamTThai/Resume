3543e29cb4e03dbc62d524c6f47c7dc01c218811
Nam Thai <nthai@ciosolutions.com>
2015-09-03 14:56:00 -0700

    All data are on the page

---
+++ public/javascripts/ready.js
@@ -0,0 +1,16 @@
+document.addEventListener("WebComponentsReady", function() {
+  $.get('/data', function(data) {
+    var files = JSON.parse(data);
+    files.forEach(function(file) {
+      $.getJSON(file, function(data) {
+        var component = data.component;
+        var parent = data.parent;
+        data.data.forEach(function(d) {
+          var object = document.createElement(component);
+          object.initialize(d);
+          $(parent).append(object);
+        });
+      });
+    });
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/data', function(req, res) {
+  var fs = require('fs');
+  fs.readdir('./data', function(err, files) {
+    res.send(JSON.stringify(files));
+  });
+});
+
 module.exports = router;
